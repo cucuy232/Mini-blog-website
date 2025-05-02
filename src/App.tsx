e63c5caf473460b95e5a43543a144f5e2265a932
cucuy232 <jushahaha10@gmail.com>
2025-05-02 20:53:10 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Index from "./pages/Index";
+import Explore from "./pages/Explore";
+import PostDetail from "./pages/PostDetail";
+import NewPost from "./pages/NewPost";
+import MyPosts from "./pages/MyPosts";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/post/:id" element={<PostDetail />} />
+            <Route path="/new-post" element={<NewPost />} />
+            <Route path="/my-posts" element={<MyPosts />} />
+            <Route path="/edit-post/:id" element={<NewPost />} /> {/* Reuse NewPost for editing */}
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/tag/:tag" element={<Explore />} /> {/* Added tag route */}
+            <Route path="/category/:category" element={<Explore />} /> {/* Added category route */}
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </AuthProvider>
+  </QueryClientProvider>
+);
+
+export default App;
